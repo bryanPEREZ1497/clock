548c4586cb0d3011e6b0baea191c0b02264c899a
Bryan Pérez <bam.perez@yavirac.edu.ec>
2022-11-18 14:40:56 -0500

    Clock component created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Clock from './components/Clock';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          <Clock />
         </a>
       </header>
     </div>
